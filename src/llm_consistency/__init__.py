c86be21ec5c9946de31fdf52fc4741e84287b402
Mohammed Khalid <emailmokhld@gmail.com>
2026-02-20 15:39:19 +0000

    feat(03-04): wire all metric functions into public API
    - Re-export all 9 metric functions from llm_consistency package
    - Add all metric function names to __all__
    - Importable as: from llm_consistency import mca, bootstrap_ci, etc.

--- src/llm_consistency/__init__.py
+++ src/llm_consistency/__init__.py
@@ -2,6 +2,17 @@
 
 from llm_consistency._exceptions import LLMConsistencyError, ValidationError
 from llm_consistency._version import __version__, __version_tuple__
+from llm_consistency.metrics import (
+    agreement_gated_accuracy,
+    bootstrap_ci,
+    build_question_consistency_result,
+    car_curve,
+    core_index,
+    dtw_distance,
+    mca,
+    normalized_dtw,
+    trapezoidal_auc,
+)
 from llm_consistency.types import (
     KNOWN_SCORERS,
     EvaluationConfig,
@@ -32,4 +43,13 @@ __all__ = [
     "ValidationError",
     "__version__",
     "__version_tuple__",
+    "agreement_gated_accuracy",
+    "bootstrap_ci",
+    "build_question_consistency_result",
+    "car_curve",
+    "core_index",
+    "dtw_distance",
+    "mca",
+    "normalized_dtw",
+    "trapezoidal_auc",
 ]
